a851bcef015a8d3e4e5c97fbed5443164a466bad
Brad <brad@brad.tw>
2019-08-13 20:33:30 +0800

    2

--- app/src/main/java/tw/org/tcca/app/scanbarcode/MainActivity.java
+++ app/src/main/java/tw/org/tcca/app/scanbarcode/MainActivity.java
@@ -1,7 +1,11 @@
 package tw.org.tcca.app.scanbarcode;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
@@ -15,7 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    123);
+        }
+
+
         webView = findViewById(R.id.webview);
+        initWebView();
     }
 
     private void initWebView(){
